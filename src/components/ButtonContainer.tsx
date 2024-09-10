6fdcb602d47e7dbd4891914f2e992a7261e018a5
Vinicius Münchow <munchow.vinicius@scalasystems.com.br>
2024-09-10 20:18:29 -0300

    refactor: componentização

---
+++ src/components/ButtonContainer.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+
+const ButtonContainer = ({ onHomePress }: { onHomePress: () => void }) => {
+    return (
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.iconButton} onPress={onHomePress}>
+                <Image source={require('@/src/assets/images/iconex.png')} style={styles.iconImage} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconButton} onPress={onHomePress}>
+                <Image source={require('@/src/assets/images/home.png')} style={styles.iconImage} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconButton} onPress={() => {}}>
+                <Image source={require('@/src/assets/images/restart.png')} style={styles.iconImage} />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        position: 'absolute',
+        width: 120,
+        height: 50,
+        left: 26,
+        top: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    iconButton: {
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    iconImage: {
+        width: 40,
+        height: 40,
+        resizeMode: 'contain',
+    },
+});
+
+export default ButtonContainer;
---
+++ src/components/SettingsButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
+
+const SettingsButton = ({ onPress }: { onPress: () => void }) => (
+    <TouchableOpacity style={styles.botaoSettings} onPress={onPress}>
+        <Image
+            source={require('@/src/assets/images/settings_icon.png')}
+            style={styles.botaoSettingsImage}
+            resizeMode="contain"
+        />
+    </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+    botaoSettings: {
+        position: 'absolute',
+        top: 10,
+        right: '4%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        borderRadius: 50,
+        padding: 10,
+    },
+    botaoSettingsImage: {
+        width: 30,
+        height: 30,
+        resizeMode: 'contain',
+    },
+});
+
+export default SettingsButton;
